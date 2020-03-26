d0004a1ccb622ec962fd317a442186ee306f97de
mahkota <mahkota@upi.edu>
2020-03-26 16:40:14 +0700

    Minor fix hapus navbar beranda.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,58 +1,88 @@
-@extends('layout/main')
-@section('title', 'Prototype Sistem Keuangan FPBS')
+<!doctype html>
+<html lang="en">
 
-@section('container')
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<div class="container d-flex align-items-center justify-content-center min-vh-100">
-    <div class="d-flex justify-content-center">
-        <div class="row">
-            <div class="col">
-                <div class="card mb-3 shadow h-100" style="width: 18rem;">
-                    <img src="{{ asset('img/fakultas.png') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Portal Fakultas</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in.</p>
-                        {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
-                    </div>
-                    <div class="card-footer">
-                        <div class="d-flex justify-content-end">
-                            <a href="#" class="btn btn-outline-dark">Buka</a>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    {{-- <title>Prototype Sistem Keuangan FPBS</title> --}}
+    <title>Prototype Sistem Keuangan FPBS</title>
+</head>
+
+<body>
+
+
+    <div class="container d-flex align-items-center justify-content-center min-vh-100">
+        <div class="d-flex justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <div class="card mb-3 shadow h-100" style="width: 18rem;">
+                        <img src="{{ asset('img/fakultas.png') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Portal Fakultas</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                in.</p>
+                            {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+                        </div>
+                        <div class="card-footer">
+                            <div class="d-flex justify-content-end">
+                                <a href="#" class="btn btn-outline-dark">Buka</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card mb-3 shadow h-100" style="width: 18rem;">
-                    <img src="{{ asset('img/departemen-prodi.png') }}" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Portal Departemen & Prodi</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in.</p>
-                        {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
-                    </div>
-                    <div class="card-footer">
-                        <div class="d-flex justify-content-end">
-                            <a href="{{url('/dp')}}" class="btn btn-outline-dark">Buka</a>
+                <div class="col">
+                    <div class="card mb-3 shadow h-100" style="width: 18rem;">
+                        <img src="{{ asset('img/departemen-prodi.png') }}" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Portal Departemen & Prodi</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                in.</p>
+                            {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+                        </div>
+                        <div class="card-footer">
+                            <div class="d-flex justify-content-end">
+                                <a href="{{url('/dp')}}" class="btn btn-outline-dark">Buka</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<style>
-    .card img {
-        object-fit: contain;
-        height: 122px;
-        padding: 22px;
-        /* opacity: 0.8; */
-        /* background-color: #979797; */
-    }
+    <style>
+        .card img {
+            object-fit: contain;
+            height: 122px;
+            padding: 22px;
+            /* opacity: 0.8; */
+            /* background-color: #979797; */
+        }
 
-    .card-img-top {
-        border-radius: 0;
-        background-color: #efebea;
-    }
-</style>
+        .card-img-top {
+            border-radius: 0;
+            background-color: #efebea;
+        }
 
-@endsection
+    </style>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
